19025204c4ba867b4f35a09f764853a2e1fd7b8a
= <=>
2018-11-11 15:04:56 +0530

    ex20.py comments added

--- ex20.py
+++ ex20.py
@@ -1,16 +1,22 @@
+# From sys module import argv identifier
 from sys import argv
 
+#Assign all arguments for this script to two variables script and input_file
 script, input_file = argv
 
+#Define a function print_all() with a single argument and print that opened file
 def print_all(f):
     print(f.read())
 
+#Define a function rewind() to go to the start of the file.
 def rewind(f):
     f.seek(0)
 
+#Define a function print_a_line() to print the line number and that particular line
 def print_a_line(line_count, f):
     print(line_count, f.readline())
 
+#Open the file and assign it to file object current_file
 current_file = open(input_file)
 
 print("First, let's print the whole file:\n")
