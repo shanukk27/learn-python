fcf19b789d14b4d66937f0e83ea717c1d0c333fd
shanukk27 <shanukk@gmail.com>
2019-05-08 15:00:15 +0530

    Updating: May 8, 2019

--- ex20.py
+++ ex20.py
@@ -1,39 +1,38 @@
-#From the sys module import argv identifier
+#From sys module import argv variable
 from sys import argv
 
-#Assign two variables script andn input_file to the aguments
+#Assign the argument variable to two variables script which is the script name and input_file which is the file name to process
 script, input_file = argv
 
-# Here, we define a function print_all() with a single argument. On the next line, we read the file
+#Define a function print_all() and read the file.
 def print_all(f):
     print(f.read())
 
-# We define a function rewind() to seek to the zeroeth position of the file we might have opened
+#Define a function rewind and seek to seek to the start of the file
 def rewind(f):
     f.seek(0)
 
-#We define a function to print the line number and print each lines from start starting with line 1 since we have already seeked to zeroeth position
+#Define a function 
 def print_a_line(line_count, f):
-    print(line_count, f.readline(), end = "")
+    print(line_count, f.readline())
 
-#Open the file input_file which we inputted through command line and assign the file descriptor to current_file
 current_file = open(input_file)
-print("First, let's print the whole file:\n")
 
-#Run function print_all
+print("First, let's print the whole file")
+
 print_all(current_file)
 
-print("Now, let's rewind, kind of like a tape.")
+print("Now, let's rewind, kind of like a tape")
 
 rewind(current_file)
 
-print("let's print three lines:")
+print("Let's print three lines")
 
-current_line = 1
+current_line =  1
 print_a_line(current_line, current_file)
 
-current_line += 1
-print_a_line(current_line, current_file)
+current_line = current_line + 1
+print_a_line(current_line, current_file )
 
-current_line += 1
+current_line = current_line + 1
 print_a_line(current_line, current_file)
